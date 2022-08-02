c21b871a60edc07ba8c0c0300f0efaf888ce0622
Saivineeth Dachepally <saivineeth.dachepally@valuemomentum.com>
2022-08-02 15:29:47 +0530

    im commiting my work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { HeaderComponent } from './header/header.component';
+import { ListTasksComponent } from './list-tasks/list-tasks.component';
+import { EdittaskComponent } from './edittask/edittask.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddTaskComponent,
+    ForgotPasswordComponent,
+    ListUsersComponent,
+    ViewProfileComponent,
+    
+    ChangePasswordComponent,
+    HeaderComponent,
+    ListTasksComponent,
+    EdittaskComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
